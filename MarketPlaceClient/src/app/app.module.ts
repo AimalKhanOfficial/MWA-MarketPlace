b90e90d93038fd760c261be17360ddd62b0ba920
jawad <jawad.turk@hotmail.com>
2018-07-14 12:21:27 -0500

    created admin module

--- MarketPlaceClient/src/app/app.module.ts
+++ MarketPlaceClient/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-
+import { AdminModuleModule } from './admin-module/admin-module.module';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+const My_Routes = [
+  { path: 'admin', module: AdminModuleModule }
+]
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(My_Routes),
+    AdminModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
