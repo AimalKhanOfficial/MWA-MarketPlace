b2725be3fd0a70e43b4be8f5cc742721b8a16ae1
lwinaung <laung@mum.edu>
2018-07-15 18:39:06 -0500

    Add post route.
    Add post route.

--- MarketPlaceServer/app.js
+++ MarketPlaceServer/app.js
@@ -10,6 +10,7 @@ require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var postsRouter = require('./routes/posts');
 
 var app = express();
 
@@ -22,10 +23,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ MarketPlaceServer/routes/posts.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+var connection = require('../dbconnection/dbconfig');
+
+router.get('/', (req, res, next)=> {
+  var posts = connection.Post.find({},function(err, list){
+    if(err) throw err;
+    return res.json(list);
+  })
+})
+
+module.exports = router;
