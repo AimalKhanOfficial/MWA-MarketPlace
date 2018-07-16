9849ea24ff75a482ad81139aa468da4d04250d54
abdelrahman91 <38641993+abdelrahman91@users.noreply.github.com>
2018-07-15 21:39:31 -0500

    create post api

--- MarketPlaceServer/app.js
+++ MarketPlaceServer/app.js
@@ -32,7 +32,7 @@ app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/posts', postsRouter);
+app.use('/api', postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
