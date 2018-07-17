3509742d0afea341bff8140ff4349e41a78fc232
jawad <jawad.turk@hotmail.com>
2018-07-16 22:45:14 -0500

    adjusted some stuff

--- MarketPlaceServer/routes/posts.js
+++ MarketPlaceServer/routes/posts.js
@@ -5,20 +5,14 @@ const { check, validationResult } = require('express-validator/check');
 var connection = require('../dbconnection/dbconfig');
 
 router.get('/posts', (req, res, next) => {
-<<<<<<< HEAD
-  var query = connection.Post.find({});
-  // .select({ "title": 1, "user_name": 1, "description": 1, "price": 1, "_id": 0 });
-=======
   // var query = connection.Post.find({}).select({ "title": 1, "user_name": 1, "description": 1, "price": 1, "_id": 1 });
->>>>>>> df8a924327f5e8ba2eaa413e074e015ee43312ef
 
-  connection.Post.find({},function (err, list) {
+  connection.Post.find({}, function (err, list) {
     if (err) throw err;
     return res.json(list);
   })
 })
 
-<<<<<<< HEAD
 router.get("/posts/activate/:id", (req, res, next) => {
 
 
@@ -42,18 +36,6 @@ router.get('/users', (req, res, next) => {
     return res.json(list);
   })
 })
-=======
-router.get('/posts/:id', (req, res, next) => {
-
-  // var query = connection.Post.find({}).findOne({ '_id': req.params.id }).select({ "title": 1, "user_name": 1, "description": 1, "price": 1, "_id": 1 });
-
-  connection.Post.find({}).findOne({ '_id': req.params.id }, function (err, data) {
-    if (err) return res.status(500).send(ERROR);
-    return res.json(data);
-  })
-});
-
->>>>>>> df8a924327f5e8ba2eaa413e074e015ee43312ef
 router.route('/posts/add')
   .post(jsonParser, function (req, res, next) {
 
