f58aa735df7ee0a11a1567ae23caafb30c6d3ef7
abdelrahman91 <38641993+abdelrahman91@users.noreply.github.com>
2018-07-17 07:46:27 -0500

    upload images to google cloud

--- MarketPlaceServer/routes/files.js
+++ MarketPlaceServer/routes/files.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 var jsonParser = express.json();
 var connection = require('../dbconnection/dbconfig');
-var firebase = require("firebase");
+const storage = require('@google-cloud/storage');
 var multer = require('multer');
 var path = require('path');
-
+var fs = require('fs');
 
 //---Upload----//
-var storage = multer.diskStorage({
+var myStorage = multer.diskStorage({
   // destination
   destination: function (req, file, cb) {
     cb(null, './public/uploads/')
@@ -17,7 +17,7 @@ var storage = multer.diskStorage({
     cb(null, file.originalname);
   }
 });
-var upload = multer({ storage: storage });
+var upload = multer({ storage: myStorage });
 //////
 
 
@@ -32,106 +32,60 @@ router.get('/file-upload', (req, res, next) => {
 router.route('/file-upload')
   .post(upload.array("uploads[]", 12), function (req, res) {
     console.log('files', req.files);
-    res.send(req.files);
+    uploadFile(req.files, res);
   });
 
-
-function uploadFile(pObj, pRes) {
-
-  console.log('****************** ADDDDD *****************')
-
-  // Initialize Firebase
-  // TODO: Replace with your project's customized code snippet
-  var config = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    databaseURL: process.env.databaseURL,
-    //storageBucket: process.env.storageBucket,
-    projectId: process.env.projectId,
-    messagingSenderId: process.env.messagingSenderId
-  };
-  firebase.initializeApp(config);
+function uploadFile(files, res) {
 
 
-  //////////////////////////////////////////////////////
+  var gcs = storage({
+    projectId: 'mwamp-cffcf',
+    keyFilename: 'uploads/congfig/mwamp-e9de3dbb063a.json'
+  });
 
-  // File or Blob named mountains.jpg
-  var file = "";
+  // Reference an existing bucket.
+  var bucket = gcs.bucket('mwamp-cffcf.appspot.com');
 
-  // Create the file metadata
-  var metadata = {
-    contentType: 'image/jpeg'
-  };
+ 
+  for (let i = 0; i < files.length; i++) {
 
-  // Upload file and metadata to the object 'images/mountains.jpg'
-  var uploadTask = storageRef.child('images/' + file.name).put(file, metadata);
-
-  // Listen for state changes, errors, and completion of the upload.
-  uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-    function (snapshot) {
-      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log('Upload is ' + progress + '% done');
-      switch (snapshot.state) {
-        case firebase.storage.TaskState.PAUSED: // or 'paused'
-          console.log('Upload is paused');
-          break;
-        case firebase.storage.TaskState.RUNNING: // or 'running'
-          console.log('Upload is running');
-          break;
+    // Upload a local file to a new file to be created in your bucket.
+    bucket.upload('uploads/'+files.originalname, function (err, file) {
+      if (!err) {
+        if (i == files.length - 1) {
+          console.log("Upload Done")
+          res.json({ status: 1 })
+        }
       }
-    }, function (error) {
+      else {
+        console.log(err)
+        console.log("Upload Fail")
+        res.json({ "status": 0 })
 
-      // A full list of error codes is available at
-      // https://firebase.google.com/docs/storage/web/handle-errors
-      switch (error.code) {
-        case 'storage/unauthorized':
-          // User doesn't have permission to access the object
-          console.log("unauthorized")
-          break;
-        case 'storage/canceled':
-          // User canceled the upload
-          console.log("canceled")
-          break;
-        case 'storage/unknown':
-          console.log("unknown")
-          // Unknown error occurred, inspect error.serverResponse
-          break;
       }
-    }, function () {
-      // Upload completed successfully, now we can get the download URL
-      uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
-        console.log('File available at', downloadURL);
-      });
     });
+
+  }
+
+
 
 }
 
 function downloadFile() {
 
-  // Create a reference to the file we want to download
-  var starsRef = storageRef.child('images/stars.jpg');
-  // Get the download URL
-  starsRef.getDownloadURL().then(function (url) {
-    // Insert url into an <img> tag to "download"
-  }).catch(function (error) {
 
-    // A full list of error codes is available at
-    // https://firebase.google.com/docs/storage/web/handle-errors
-    switch (error.code) {
-      case 'storage/object_not_found':
-        // File doesn't exist
-        break;
-      case 'storage/unauthorized':
-        // User doesn't have permission to access the object
-        break;
-      case 'storage/canceled':
-        // User canceled the upload
-        break;
-      case 'storage/unknown':
-        // Unknown error occurred, inspect the server response
-        break;
-    }
-  });
+  // // Download a file from your bucket.
+  // bucket.file('giraffe.jpg').download({
+  //   destination: '/photos/zoo/giraffe.jpg'
+  // }, function(err) {});
+
+  // // Streams are also supported for reading and writing files.
+  // var remoteReadStream = bucket.file('giraffe.jpg').createReadStream();
+  // var localWriteStream = fs.createWriteStream('/photos/zoo/giraffe.jpg');
+  // remoteReadStream.pipe(localWriteStream);
+
+  // var localReadStream = fs.createReadStream('/photos/zoo/zebra.jpg');
+  // var remoteWriteStream = bucket.file('zebra.jpg').createWriteStream();
+  // localReadStream.pipe(remoteWriteStream);
 }
 module.exports = router;
