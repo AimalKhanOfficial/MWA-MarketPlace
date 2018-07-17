b9cafc04dd5d470d43f709be9596dbef12be1434
jawad <jawad.turk@hotmail.com>
2018-07-16 23:09:17 -0500

    fix some stuff

--- MarketPlaceServer/routes/posts.js
+++ MarketPlaceServer/routes/posts.js
@@ -6,7 +6,7 @@ var connection = require('../dbconnection/dbconfig');
 
 router.get('/posts', (req, res, next) => {
   // var query = connection.Post.find({}).select({ "title": 1, "user_name": 1, "description": 1, "price": 1, "_id": 1 });
-
+  //hey
   connection.Post.find({}, function (err, list) {
     if (err) throw err;
     return res.json(list);
