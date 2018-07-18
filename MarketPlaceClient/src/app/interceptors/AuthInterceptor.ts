dbbcf357dec34c904250700f94662b7ed65edd11
abdelrahman91 <38641993+abdelrahman91@users.noreply.github.com>
2018-07-17 23:55:14 -0500

    discluding from interceptor

--- MarketPlaceClient/src/app/interceptors/AuthInterceptor.ts
+++ MarketPlaceClient/src/app/interceptors/AuthInterceptor.ts
@@ -8,14 +8,17 @@ export class AuthInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         let token = sessionStorage.getItem("token");
         console.log("Inside Interceptor", token);
-        if(token!=null){
-            req = req.clone({
-                setHeaders: {
-                    'Content-Type': 'application/json; charset=utf-8',
-                    'Accept': 'application/json',
-                    'Authorization': `Bearer ${token}`,
-                },
-            });
+        if (token != null) {
+            if (req.url != "http://localhost:3000/imgapi/file-upload") {
+                req = req.clone({
+                    setHeaders: {
+                        'Content-Type': 'application/json; charset=utf-8',
+                        'Accept': 'application/json',
+                        'Authorization': `Bearer ${token}`,
+                    },
+                });
+            }
+
         }
         return next.handle(req);
     }
